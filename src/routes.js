936675259e98294ea0e5dd1001fb7e29de386e7a
Shrikrishna Dhas <shrikrishna.dhas@simplifai.ai>
2020-06-12 16:50:33 +0530

    feat: base routr

--- src/routes.js
+++ src/routes.js
@@ -3,10 +3,10 @@ import { QuestionRoutes } from './modules/question/question.route';
 import { TestRoutes } from './modules/test/test.route';
 
 function enableRoutes(app) {
-    app.use('/', function(req, res, next){res.end('Apis are working')});
     app.use('/api/dashboard', DashboardRoutes);
     app.use('/api/tests', TestRoutes);
     app.use('/api/questions', QuestionRoutes);
+    app.use('/', function(req, res, next){res.end('Apis are working')});
 }
 
 export { enableRoutes };
