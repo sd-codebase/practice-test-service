959343630c4777f579ad211330828950abf611df
Shrikrishna Dhas <shrikrishna.dhas@simplifai.ai>
2020-06-12 16:47:21 +0530

    feat: basic route

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import { QuestionRoutes } from './modules/question/question.route';
 import { TestRoutes } from './modules/test/test.route';
 
 function enableRoutes(app) {
+    app.use('/', function(req, res, next){res.end('Apis are working')});
     app.use('/api/dashboard', DashboardRoutes);
     app.use('/api/tests', TestRoutes);
     app.use('/api/questions', QuestionRoutes);
