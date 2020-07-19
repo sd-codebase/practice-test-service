05cd26d95be5ec55d856e409a140a330067826ba
Shrikrishna Dhas <shrikrishna.dhas@simplifai.ai>
2020-07-20 01:14:32 +0530

    feat: mlab database

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-export const dburl = 'mongodb://herokudev:herokudev1@ds023438.mlab.com:23438/heroku_v066m8l5';
+// export const dburl = 'mongodb://herokudev:herokudev1@ds023438.mlab.com:23438/heroku_v066m8l5';
 // let heroku_db_url = 'mongodb://herokudev:herokudev1@ds023438.mlab.com:23438/heroku_v066m8l5';
-// let mongoLab_db_url = 'mongodb://dev-account:devuser1@ds113942.mlab.com:13942/sd-practice-tests-dev';
+export const mongoLab_db_url = 'mongodb://dev-account:devuser1@ds113942.mlab.com:13942/sd-practice-tests-dev';
 // export const dburl = 'mongodb://127.0.0.1:27017/quiz';
 // export const dburl = 'mongodb://127.0.0.1:27017/testquiz';
--- src/modules/test/test.model.js
+++ src/modules/test/test.model.js
@@ -18,10 +18,31 @@ let TestSchema = new Schema({
     createdAt: {type: Date, default: Date.now()},
     updatedAt: {type: Date, default: Date.now()},
     isPredefined: {type:Boolean, default:false},
-    testName:{type:String}
+    testName:{type:String},
+    instructions:{type: Schema.Types.Mixed},
 });
 
 
 const TestModel = mongoose.model('Test', TestSchema);
 
 export {TestModel};
+
+const ob = {
+    "Section 1": {
+        "questions": [1,2,3,4,5],
+        "instruction": "Question 1-5, each question allotted maximum 4 marks. Correct answer will give you 4 marks, and wrong answer will give -1 marks."
+    },
+    "Section 2": {
+        "questions": [6,7,8,9,10],
+        "instruction": "Question 6-10, each question allotted maximum 4 marks. Correct answer will give you 4 marks, and wrong answer will give -1 marks."
+    },
+    "Section 3": {
+        "questions": [11,12,13,14,15],
+        "instruction": "Question 11-15, each question allotted maximum 4 marks. Correct answer will give you 4 marks, and wrong answer will give -1 marks."
+    },
+    "Section 4": {
+        "questions": [16,17,18,19,20],
+        "instruction": "Question 16-20, each question allotted maximum 4 marks. Correct answer will give you 4 marks, and wrong answer will give -1 marks."
+    }
+};
+
