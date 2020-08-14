954a242afba5a5b505385a166fd27a5e376e42f4
Shrikrishna Dhas <shrikrishna.dhas@simplifai.ai>
2020-08-15 02:53:16 +0530

    debug

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -36,14 +36,13 @@ export const handleAuth = app => {
         if (httpMethod !== 'OPTIONS' && isAuthRequired(httpMethod, apiUrl)) {
             let authHeader = req.header('Authorization');
             let sessionID = authHeader && authHeader.split(' ')[1];
-            console.log(sessionID);
             if (sessionID) {
                 let url = `https://test-for-all-services.herokuapp.com/api/users/get-user-for-verification`;
                 try {
                     const request = await axios.post(url,{sessionID});
                     
                     console.log(request.data, request.status);
-                    if(request.status !== 1) {
+                    if(request.data.status !== 1) {
                         throw {re: request}
                     }
                 } catch (e) {
